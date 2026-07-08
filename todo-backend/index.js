af805289a12bc1608adcaf05bc1c023ab6606c0a
Sabrina Altahan <sabrinaltahan157@gmail.com>
2026-07-08 22:32:23 +0400

    add GET endpoint for todos

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -7,8 +7,14 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+let todos = [];
+
 app.get("/", (req, res) => {
   res.json({ message: "Todo API is running" });
+});
+
+app.get("/todos", (req, res) => {
+  res.json(todos);
 });
 
 app.listen(PORT, () => {
